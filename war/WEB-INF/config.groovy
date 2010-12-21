c90b8445abaacfe0771fa4a4f8dd20351d5a9891
rainboyan <rainboyan@9e61b89f-7907-8a86-4aba-93c1c01e39e9>
2010-12-21 12:47:21 +0000

    Ratte: add douban config
    git-svn-id: http://ratte.googlecode.com/svn/trunk@14 9e61b89f-7907-8a86-4aba-93c1c01e39e9

--- war/WEB-INF/config.groovy
+++ war/WEB-INF/config.groovy
@@ -13,3 +13,11 @@ sina {
     oauth.token = "a6708cc45e72469xe3a6720hwf9c0cwq"
     oauth.token_secret = "ea0e6b91we0e8e7e58a78v42ta13ae7at"
 }
+
+douban {
+	oauth.consumer_key = "08ooooooooo90a19992e13ewdh8ee"
+	oauth.consumer_secret = "4ewaf9htfb3a452"
+    oauth.token = "c18246812797da4464f0483766efg"
+    oauth.token_secret = "35ge0c46e934167j"
+    oauth.callback = "http://rainboyan.appspot.com/"
+}
