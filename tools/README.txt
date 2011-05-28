34b473559abeb164ef46197abb6d67de9ad63211
rainboyan <rainboyan@9e61b89f-7907-8a86-4aba-93c1c01e39e9>
2011-05-28 09:39:14 +0000

    Ratte: add oauth tools directory
    git-svn-id: http://ratte.googlecode.com/svn/trunk@24 9e61b89f-7907-8a86-4aba-93c1c01e39e9

---
+++ tools/README.txt
@@ -0,0 +1,18 @@
+README
+---------------------------------
+
+1 需要安装Java 6
+2 安装好后，在命令行运行
+> run org.scribe.example.DoubanExample
+
+或者
+> run org.scribe.example.SinaExample
+
+分别获取访问的Token和secret
+
+
+默认的API key是Ratte应用提供，你可以修改为自己的。
+
+
+rain
+2011.5.28
---
+++ tools/run.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+rem set JAVA_HOME=C:\SDKs\jdk1.6.0
+
+rem %~dp0 is the expanded pathname of the current script under NT
+set LOCAL_TOOLS_HOME=
+if "%OS%"=="Windows_NT" set LOCAL_TOOLS_HOME=%~dp0
+
+set DEFAULT_PATH=%PATH%
+set DEFAULT_CLASSPATH=%CALCLASSPATH%
+
+set BINDIR=%LOCAL_TOOLS_HOME%\
+set LIBDIR=%LOCAL_TOOLS_HOME%\lib
+set LOCALCLASSPATH=%LIBDIR%\scribe-1.2.0.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\commons-codec-1.4.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\commons-logging-1.1.jar
+set PATH=%JAVA_HOME%\bin;%Path%;%BINDIR%
+set CLASSPATH=.;%LOCALCLASSPATH%
+@java -cp %CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8
+
+set PATH=%DEFAULT_PATH%
+set CLASSPATH=%DEFAULT_CALCLASSPATH%
+@pause
