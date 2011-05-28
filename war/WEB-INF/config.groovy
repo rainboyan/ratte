7432000b535bf3fe15952b114e9a30f284ed13b9
rainboyan <rainboyan@9e61b89f-7907-8a86-4aba-93c1c01e39e9>
2011-05-28 09:38:35 +0000

    git-svn-id: http://ratte.googlecode.com/svn/trunk@23 9e61b89f-7907-8a86-4aba-93c1c01e39e9

--- war/WEB-INF/config.groovy
+++ war/WEB-INF/config.groovy
@@ -1,23 +1,23 @@
 // twitter oauth
 twitter {
     username = "rainboyan"
-	oauth.consumer_key = "5xEg1VuinKkKiwK4jkEUA"
-	oauth.consumer_secret = "aTOmGbKuinblP5jLzhzHDsb95LpVBSf3vCFil0inyT"
-    oauth.token = "45569022-EH78EcCopnhuyTR3qMF1jAsIDRFFV6d4T1VWwLwDs"
-    oauth.token_secret = "gQbNYrTsfEGTVRWVVBLeNh1RlFpOCUYdFdkoJAqwEIa"
+	oauth.consumer_key = "2xEg1VTj03kKiwK4HLEUA"
+	oauth.consumer_secret = "xTOmGbVxLW2lP5jLzhzHDsb95LpVBSf3vCFil0i1H0"
+    oauth.token = ""
+    oauth.token_secret = ""
 }
 
 sina {
-	oauth.consumer_key = "23708456"
-	oauth.consumer_secret = "24fb98a12f8d6029e169efe92acr4d9b"
-    oauth.token = "a6708cc45e72469xe3a6720hwf9c0cwq"
-    oauth.token_secret = "ea0e6b91we0e8e7e58a78v42ta13ae7at"
+	oauth.consumer_key = "947084098"
+	oauth.consumer_secret = "0eb98a12f8d6029e169efe92ac6ddc9b"
+    oauth.token = ""
+    oauth.token_secret = ""
 }
 
 douban {
-	oauth.consumer_key = "08ooooooooo90a19992e13ewdh8ee"
-	oauth.consumer_secret = "4ewaf9htfb3a452"
-    oauth.token = "c18246812797da4464f0483766efg"
-    oauth.token_secret = "35ge0c46e934167j"
+	oauth.consumer_key = "088864b1009ad90a19992e1301d08b1e"
+	oauth.consumer_secret = "98caf9191fb3a4f2"
+    oauth.token = ""
+    oauth.token_secret = ""
     oauth.callback = "http://rainboyan.appspot.com/"
 }
